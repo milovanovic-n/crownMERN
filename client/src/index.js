fb026fd97114d5f8fc19f22b3d3d8d041829266f
Nemanja <nemanja94email@gmail.com>
2021-01-09 16:51:12 +0100

    **

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql"
+  uri: "https://crown-store110.herokuapp.com/graphql"
 });
 
 const authLink = setContext(() => {
